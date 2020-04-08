d797214974851e28ec84f0c93a77acb8209d1025
seungdols <seungdols0822@gmail.com>
2020-04-08 17:51:29 +0900

    Add companion object

--- start_kotlin/src/main/kotlin/com/seungdols/interface.practice/Person.kt
+++ start_kotlin/src/main/kotlin/com/seungdols/interface.practice/Person.kt
@@ -13,13 +13,18 @@ interface Signatory {
 open class Person(val name: String, var age: Int, var isMarried: Boolean = false) : Signatory {
 
     override fun sign() = println("$name aged $age can sign documents. (and their marital status is $isMarried)")
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val p = Person("seungdols", 30, true)
+            p.sign()
+            p.age = 28
+            p.sign()
+        }
+    }
 }
 
 class Student(name: String, age: Int) : Person(name, age)
 
-fun main(args: Array<String>) {
-    val p = Person("seungdols", 30, true)
-    p.sign()
-    p.age = 28
-    p.sign()
-}
+
