9987e656adc9ca233d219846505e47f88f3ae05d
seungdols <seungdols0822@gmail.com>
2020-04-08 17:41:30 +0900

    Practice init

--- start_kotlin/src/main/kotlin/com/seungdols/interface.practice/Person.kt
+++ start_kotlin/src/main/kotlin/com/seungdols/interface.practice/Person.kt
@@ -10,11 +10,18 @@ interface Signatory {
     fun sign()
 }
 
-class Person : Signatory {
-    override fun sign()  = println("I can sign document.")
+open class Person(val name: String, var age: Int) : Signatory {
+    init {
+        if (name == "seungdols" && age < 54) throw Exception("Invalid age")
+    }
+    override fun sign()  = println("$name aged $age can sign documents.")
 }
 
+class Student(name: String, age: Int) : Person(name, age)
+
 fun main(args: Array<String>) {
-    val p = Person()
+    val p = Person("seungdols", 30)
+    p.sign()
+    p.age = 28
     p.sign()
 }
