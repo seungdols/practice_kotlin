d975351bfe3c9a4dc8eb3cbdf1dde38e8b620447
seungdols <seungdols0822@gmail.com>
2020-04-08 17:46:38 +0900

    Using constructor

--- start_kotlin/src/main/kotlin/com/seungdols/interface.practice/Person.kt
+++ start_kotlin/src/main/kotlin/com/seungdols/interface.practice/Person.kt
@@ -11,16 +11,18 @@ interface Signatory {
 }
 
 open class Person(val name: String, var age: Int) : Signatory {
-    init {
-        if (name == "seungdols" && age < 54) throw Exception("Invalid age")
+    var isMarried: Boolean = false
+    constructor(name: String, age: Int, isMarried: Boolean): this(name, age) {
+        this.isMarried = isMarried
     }
-    override fun sign()  = println("$name aged $age can sign documents.")
+
+    override fun sign()  = println("$name aged $age can sign documents. (and their marital status is $isMarried)")
 }
 
 class Student(name: String, age: Int) : Person(name, age)
 
 fun main(args: Array<String>) {
-    val p = Person("seungdols", 30)
+    val p = Person("seungdols", 30, true)
     p.sign()
     p.age = 28
     p.sign()
